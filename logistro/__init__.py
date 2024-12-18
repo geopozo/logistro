c18605ab98776aa9868af6aeba378e719feaeca4
Andrew Pikul <ajpikul@gmail.com>
2024-12-18 10:21:58 -0500

    Reorganize imports

--- logistro/__init__.py
+++ logistro/__init__.py
@@ -1,18 +1,18 @@
-from logistro.custom_logging import betterConfig
-from logistro.custom_logging import coerce_logger
-from logistro.custom_logging import DEBUG2
-from logistro.custom_logging import getLogger
-from logistro.custom_logging import getPipeLogger
-from logistro.custom_logging import human_formatter
-from logistro.custom_logging import set_human
-from logistro.custom_logging import set_structured
-from logistro.custom_logging import structured_formatter
+from .custom_logging import betterConfig
+from .custom_logging import coerce_logger
+from .custom_logging import DEBUG2
+from .custom_logging import getLogger
+from .custom_logging import getPipeLogger
+from .custom_logging import human_formatter
+from .custom_logging import set_human
+from .custom_logging import set_structured
+from .custom_logging import structured_formatter
 
 __all__ = [
     DEBUG2,
+    betterConfig,
     getLogger,
     getPipeLogger,
-    betterConfig,
     set_human,
     set_structured,
     human_formatter,
